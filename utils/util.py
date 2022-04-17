160b5f2344eb35ff54ef55222d38718b77f2a5b4
NicNel <100981393+NicNel@users.noreply.github.com>
2022-04-17 23:54:50 +0300

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -59,16 +59,18 @@ def safe_register_class(cls):
         #logger.error("[appleseed] ERROR: Failed to register class {0}: {1}".format(cls, e))
 
 def deleteAllInFolder(folder):
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                print("delete file: ", file_path)
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    pass
+    #comment this for now
+    #for filename in os.listdir(folder):
+    #    file_path = os.path.join(folder, filename)
+    #    try:
+    #        if os.path.isfile(file_path) or os.path.islink(file_path):
+    #            print("delete file: ", file_path)
+    #            os.unlink(file_path)
+    #        elif os.path.isdir(file_path):
+    #            shutil.rmtree(file_path)
+    #    except Exception as e:
+    #        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def getFileName(path):
     head, tail = os.path.split(path)
