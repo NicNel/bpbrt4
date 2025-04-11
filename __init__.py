9c161e0723c7e0fe47aa6139f1b2a222085b371e
NicNel <100981393+NicNel@users.noreply.github.com>
2025-04-12 01:57:16 +0300

    Migrate the legacy type add-on to Blender 4

--- __init__.py
+++ __init__.py
@@ -1,26 +1,12 @@
-bl_info = {
-    "name": "PBRTV4_Exporter",
-    "author": "NicNel",
-    "version": (0, 1),
-    "blender": (2, 80, 0),
-    "category": "Render",
-    "location": "Info header, render engine menu",
-    "description": "PBRTV4 for Blender",
-    "warning": "",
-}
-
 import sys
 import os
 
 currDir = os.path.abspath(os.path.dirname(__file__))
-#sys.path.append(currDir)
 
 from .pbrt import registerPbrt, unregisterPbrt
 
 def register():
-    #pbrt.register()
     registerPbrt()
 
 def unregister():
-    #pbrt.unregister()
     unregisterPbrt()
